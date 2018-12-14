1eb5d6251eedd7abbd460e3f24a03f12f11833d5
Lashit Jain <lashitj@labrobot2.cs.bgu.ac.il>
2018-12-14 21:07:12 +0200

    OpStackNode code added

--- src/search/operator_id.h
+++ src/search/operator_id.h
@@ -5,6 +5,13 @@
 
 #include <iostream>
 
+#include <vector>
+#include <unordered_set>
+#include <unordered_map>
+#include <utility>
+
+#include "state_id.h"
+
 /*
   OperatorIDs are used to define an operator that belongs to a given
   planning task. These IDs are meant to be compact and efficient to use.
@@ -57,5 +64,55 @@ inline void feed(HashState &hash_state, OperatorID id) {
     feed(hash_state, id.hash());
 }
 }
+
+class OpStackNode{
+
+  // stores the value of the operator it represents
+  int op_id;
+  
+  OpStackNode* par;
+  std::unordered_map<int, OpStackNode*> children;
+
+  // represents stack size
+  int depth;
+  // represents sum of g_values of all operator
+  int cost;
+  
+  // stores state for duplicate detection
+  std::unordered_set<StateID> state_storage;
+
+public:
+  OpStackNode(int operator_id, OpStackNode* parent, int op_cost=0);
+
+  int get_operator();
+  OpStackNode* get_parent();
+  int get_depth();
+  int get_cost();
+
+  /* generates child, pair.second is false if state already requested here */
+  std::pair<OpStackNode*, bool> gen_child(int operator_id, StateID state_id, int op_cost);
+
+  // returns false if the state if already there, else true
+  bool store_state(StateID state_id);
+};
+
+namespace fwdbwd{
+    using FwdbwdOps = std::pair<int, bool>;
+    class FwdbwdNode{
+        StateID id;
+        int op_id;
+        OpStackNode* op_stack;
+        int state_g_value;
+    public:
+        FwdbwdNode(StateID state_id, int operator_id, OpStackNode* op_stack_node, int g_value);
+        StateID get_state() const {return id;}
+        int get_operator() const {return op_id;}
+        OpStackNode* get_stack_pointer() const {return op_stack;}
+        int get_g() const{return state_g_value;}
+
+        bool operator<(const FwdbwdNode& rhs) const;
+    };
+}
+
 
 #endif
